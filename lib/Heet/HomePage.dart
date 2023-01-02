e45a0116bfce02bced6747a271ee56d47692629c
Heet Limbani <heetlimbani9999@gmail.com>
2023-01-02 10:48:12 +0530

    HomePage

--- lib/Heet/HomePage.dart
+++ lib/Heet/HomePage.dart
@@ -1,6 +1,112 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
+              Color.fromARGB(255, 17, 71, 234),
+              Color.fromARGB(255, 25, 100, 231),
+              Color.fromARGB(255, 66, 163, 243),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: 600,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                  color: Color.fromARGB(255, 249, 250, 250),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                  child: Text(
+                    "As Users",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.08,
+                  width: MediaQuery.of(context).size.width * 0.495,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      bottomLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                    color: null,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.01,
+                  height: MediaQuery.of(context).size.height * 0.08,
+                  decoration: BoxDecoration(color: null),
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 36),
+                  child: Text(
+                    "As Organizer",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 10),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.08,
+                  width: MediaQuery.of(context).size.width * 0.495,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                      topLeft: Radius.circular(50),
+                      bottomLeft: Radius.circular(50),
+                    ),
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+/*import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -53,3 +159,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+*/

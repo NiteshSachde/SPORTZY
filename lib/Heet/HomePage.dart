2e2f3f13085aef6f434eeb8d1608d908abce6306
NiteshSachde <20bca030@sanskareducation.org>
2023-01-16 13:10:35 +0530

    Home Page

--- lib/Heet/HomePage.dart
+++ lib/Heet/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sportzy/Nitesh/selectSport.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,158 +13,43 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      bottomNavigationBar: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Color.fromARGB(255, 17, 71, 234),
-              Color.fromARGB(255, 25, 100, 231),
-              Color.fromARGB(255, 66, 163, 243),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+          color: Color.fromARGB(255, 25, 100, 231),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 12),
+          child: GNav(
+            backgroundColor: Color.fromARGB(255, 25, 100, 231),
+            color: Colors.white,
+            activeColor: Color.fromARGB(255, 25, 100, 231),
+            tabBackgroundColor: Colors.white,
+            gap: 6,
+            padding: EdgeInsets.all(16),
+            iconSize: 32,
+            tabs: const [
+              GButton(
+                icon: Icons.search,
+                text: "Viewer",
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 25, 100, 231),
+                ),
+              ),
+              GButton(
+                icon: Icons.create,
+                text: "Organizer",
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 25, 100, 231),
+                ),
+              )
             ],
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
-                  ),
-                  color: Color.fromARGB(255, 249, 250, 250),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.01,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 60),
-                  child: Text(
-                    "As User",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  height: MediaQuery.of(context).size.height * 0.08,
-                  width: MediaQuery.of(context).size.width * 0.495,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      bottomLeft: Radius.circular(50),
-                      topRight: Radius.circular(50),
-                      bottomRight: Radius.circular(50),
-                    ),
-                    color: null,
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.01,
-                  height: MediaQuery.of(context).size.height * 0.08,
-                  decoration: BoxDecoration(color: null),
-                ),
-                GestureDetector(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 39),
-                    child: Text(
-                      "As Organizer",
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    height: MediaQuery.of(context).size.height * 0.08,
-                    width: MediaQuery.of(context).size.width * 0.48,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(50),
-                        bottomRight: Radius.circular(50),
-                        topLeft: Radius.circular(50),
-                        bottomLeft: Radius.circular(50),
-                      ),
-                      color: Colors.white,
-                    ),
-                  ),
-                  onTap: (() {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (BuildContext context) => SelectSport()));
-                  }),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.01,
-            )
-          ],
-        ),
       ),
     );
   }
 }
-/*import 'package:flutter/material.dart';
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(color: Colors.white),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.08,
-                  width: MediaQuery.of(context).size.width * 0.498,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                    ),
-                    color: Color.fromARGB(255, 23, 107, 204),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.004,
-                  height: MediaQuery.of(context).size.height * 0.08,
-                  decoration: BoxDecoration(color: Colors.white),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.08,
-                  width: MediaQuery.of(context).size.width * 0.498,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(30),
-                    ),
-                    color: Color.fromARGB(255, 23, 107, 204),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/

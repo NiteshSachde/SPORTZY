0ab2d34b222dcbf035d69adac242a851a455962c
NiteshSachde <20bca030@sanskareducation.org>
2023-01-30 11:45:43 +0530

    Connection

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:firebase_core/firebase_core.dart';
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  runApp(Sportzy());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Sportzy extends StatelessWidget {
+  const Sportzy({super.key});
 
   // This widget is the root of your application.
   @override

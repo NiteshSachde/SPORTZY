8d2e994114ae0da484a45e7b66d81f5739a32b16
Heet Limbani <heetlimbani9999@gmail.com>
2023-02-17 12:31:40 +0530

    minor change

--- lib/Nitesh/doublesBadminton.dart
+++ lib/Nitesh/doublesBadminton.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-//import 'package:sportzy/Heet/singleScoreScreen.dart';
-
 import '../Heet/doubleScoreScreen.dart';
 
 class DoublesBadminton extends StatefulWidget {
--- lib/Nitesh/singlesBadminton.dart
+++ lib/Nitesh/singlesBadminton.dart
@@ -1,9 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-//import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sportzy/Heet/singleScoreScreen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class SinglesBadminton extends StatefulWidget {
   const SinglesBadminton({Key? key}) : super(key: key);
@@ -13,7 +10,6 @@ class SinglesBadminton extends StatefulWidget {
 }
 
 class _SinglesBadminton extends State<SinglesBadminton> {
-  final _auth = FirebaseAuth.instance;
   final _formKey = GlobalKey<FormState>();
   final TextEditingController p1 = new TextEditingController();
   final TextEditingController p2 = new TextEditingController();
